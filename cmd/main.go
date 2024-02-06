e372e37a70d1c852a0be2998c28eb4b8572c6d16
santtuniskanen <santtuniska02@gmail.com>
2024-02-06 14:15:02 +0200

    received request

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+type Film struct {
+	Title    string
+	Director string
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	h1 := func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		films := map[string][]Film{
+			"Films": {
+				{Title: "The Godfather", Director: "Francis Ford Coppola"},
+				{Title: "Blade Runner", Director: "Ridley Scott"},
+				{Title: "The Thing", Director: "John Carpenter"},
+			},
+		}
+		tmpl.Execute(w, films)
+	}
+	h2 := func(w http.ResponseWriter, r *http.Request) {
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("name")
+	}
+
+	http.HandleFunc("/", h1)
+	http.HandleFunc("/add-film/", h2)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
