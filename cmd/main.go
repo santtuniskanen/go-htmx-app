cb35d3b70da9c20c4333adcb0ee036cc9c8bb3cd
santtuniskanen <santtuniska02@gmail.com>
2024-02-06 13:36:12 +0200

    added go.mod and index.html

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/santtuniskanen/go-htmx-app
+
+go 1.21.6
